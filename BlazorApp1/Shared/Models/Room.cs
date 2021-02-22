1cf5f20d238c5936121e71409bfafa95a7456cd8
YGeller <yevgeller+git@gmail.com>
2021-02-21 22:12:31 -0600

    Add project files.

---
+++ BlazorApp1/Shared/Models/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace BlazorApp1.Shared.Models
+{
+    [DebuggerDisplay("{ForDisplay}")]
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MaxCapacity { get; set; }
+        public string Description { get; set; }
+        public string ForDisplay()
+        {
+            return $"{Id}-{Name}-{MaxCapacity}-{Description}";
+        }
+    }
+}
