4c821857f670a1b8673be3471014240d7d29cc14
YGeller <yevgeller+git@gmail.com>
2021-02-23 22:14:33 -0600

    Adding list of dummy users in a table.

---
+++ BlazorApp1/Shared/Models/StaticUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp1.Shared.Models
+{
+    public static class StaticUtils
+    {
+        public static string BoolToStringYesNo(bool value)
+        {
+            if (value)
+                return "Yes";
+            return "No";
+        }
+    }
+}
---
+++ BlazorApp1/Shared/Models/SystemUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp1.Shared.Models
+{
+    public class SystemUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsTeacher { get; set; }
+    }
+}
