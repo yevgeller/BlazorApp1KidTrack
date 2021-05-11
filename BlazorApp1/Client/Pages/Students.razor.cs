19dd25abeb6c83c835e5dbe9e6ca8015300cb83e
YGeller <yevgeller+git@gmail.com>
2021-05-10 22:29:43 -0500

    Working student room assignments page

--- BlazorApp1/Client/Pages/Students.razor.cs
+++ BlazorApp1/Client/Pages/Students.razor.cs
@@ -19,6 +19,10 @@ namespace BlazorApp1.Client.Pages
         [Parameter]
         public int Id { get; set; }
 
+        public int EditingThisId { get; set; }
+        private int selectedRoom;
+        public List<Room> allRooms;
+
         [Inject]
         NavigationManager nm { get; set; }
 
@@ -36,12 +40,28 @@ namespace BlazorApp1.Client.Pages
             base.OnParametersSet();
         }
 
+        void SetEditable(StudentRoom thisStudentRoom)
+        {
+            EditingThisId = thisStudentRoom.Id;
+        }
+
+        void SaveEdits(StudentRoom editableStudentRoom)
+        {
+            int newRoomId = selectedRoom;
+        }
+
+        void CancelEdit()
+        {
+            EditingThisId = -1;
+        }
+
         protected override void OnInitialized()
         {
             base.OnInitialized();
             mainDAL = new MainDAL();
-
+            EditingThisId = -1;
             allStudentRooms = mainDAL.GetAllStudentRoomAssignments();
+            allRooms = mainDAL.GetRooms();
         }
 
     }
