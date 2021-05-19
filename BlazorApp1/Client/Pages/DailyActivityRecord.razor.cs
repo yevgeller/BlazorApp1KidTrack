a6f65e98dbd8a3b175ea1088fe43f4e69cbc30e1
YGeller <yevgeller+git@gmail.com>
2021-05-18 21:29:41 -0500

    RoomDetails, starting working on student daily activity log

---
+++ BlazorApp1/Client/Pages/DailyActivityRecord.razor.cs
@@ -0,0 +1,40 @@
+﻿using BlazorApp1.FakeDAL2;
+using BlazorApp1.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Client.Pages
+{
+    public partial class DailyActivityRecord
+    {
+        MainDAL mainDAL;
+
+        //List<StudentRoom> allStudentRooms;
+
+        //List<Room> rooms;
+
+        [Parameter]
+        public int StudentId { get; set; }
+
+        //public int EditingThisId { get; set; }
+        //private int selectedRoom;
+        //public List<Room> allRooms;
+
+        [Inject]
+        NavigationManager nm { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            StudentId = StudentId;
+            base.OnParametersSet();
+        }
+        protected override void OnInitialized()
+        {
+            mainDAL = new MainDAL();
+            base.OnInitialized();
+        }
+    }
+}
